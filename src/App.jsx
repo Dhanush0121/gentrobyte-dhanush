7a397f4a8a2ca8d226b4747e1e9f4cf596dacdb2
Dhanush0121 <dhanushfrim@gmail.com>
2026-08-22 13:58:42 +0530

    added react js

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Hello World!</h1>
+      <p>My React app is deployed on Vercel 🚀</p>
+    </div>
+  );
+}
